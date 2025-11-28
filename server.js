338de5fd697815416aa002f79a70368fa6253d35
SethNo1 <airdropp9999@gmail.com>
2025-11-28 19:04:25 +0700

    server.js

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
+import http from "http";
 import { WebSocketServer } from "ws";
 
 const port = process.env.PORT || 3000;
 
-const wss = new WebSocketServer({ port });
-
-wss.on("connection", (ws) => {
-    console.log("Client connected");
-
-    ws.on("message", (message) => {
-        console.log("Received:", message.toString());
-        ws.send("Server echo: " + message.toString());
-    });
-
-    ws.on("close", () => {
-        console.log("Client disconnected");
-    });
+// HTTP server minimal untuk Railway
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("WebSocket server is running.");
 });
 
-console.log("WebSocket server running on port " + port);
+// Buat WebSocket server di atas HTTP server
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log("Received:", message.toString());
+    ws.send("Server echo: " + message.toString());
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Jalankan HTTP + WS server
+server.listen(port, () => {
+  console.log("Server running on port " + port);
+});
